cd3f07d1f18ece57930ca608972107b342cac138
Gamerlagan-lag <70490317+Gamerlagan-lag@users.noreply.github.com>
2021-06-05 10:44:02 -0700

    Update mn.js

--- Commands/Moderation/mn.js
+++ Commands/Moderation/mn.js
@@ -5,5 +5,5 @@ code: `
 $changeNickname[$mentioned[1];Moderated Nickname $random[11;99999]]
 $deletecommand
 $suppressErrors[Sorry but something went wrong! Please DM LaganYT#0001]
-$onlyIf[$hasRole[$authorID;$getVar[staffrole]!=true;]`
+$onlyIf[$hasRole[$authorID;$getVar[staffrole]]!=true;]`
 })
