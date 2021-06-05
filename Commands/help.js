9487ad809fed276aea2a8253c028c3237b1b6380
Gamerlagan-lag <70490317+Gamerlagan-lag@users.noreply.github.com>
2021-06-05 10:42:56 -0700

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -8,7 +8,7 @@ $description[Mod Commands
 --
 \`warn, kick, ban, mute, unmute, mn, sm, purge, rw, lockdown, hide\`
 --]
-$onlyIf[$hasRole[$authorID;$getVar[staffrole]!=true;]
+$onlyIf[$hasRole[$authorID;$getVar[staffrole]]==true;]
 $elseIf[$message!=;]
 $color[RED]
 $title[Help]
@@ -16,7 +16,7 @@ $description[Name: $commandInfo[$message;name]
 Ussage: $commandInfo[$message;ussage]
 Description: $commandInfo[$message;description]
 Aliases: $commandInfo[$message;aliases]]
-$onlyIf[$hasRole[$authorID;$getVar[staffrole]!=true;]
+$onlyIf[$hasRole[$authorID;$getVar[staffrole]]==true;]
 $endelseIf
 $endif
 $suppressErrors[Sorry but something went wrong! Please DM LaganYT#0001]
