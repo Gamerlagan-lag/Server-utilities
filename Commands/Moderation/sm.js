8e1c4708884b9dd6a81841f9c4f0d5581134925c
Gamerlagan-lag <70490317+Gamerlagan-lag@users.noreply.github.com>
2021-06-08 14:45:04 -0700

    Update sm.js

--- Commands/Moderation/sm.js
+++ Commands/Moderation/sm.js
@@ -1,14 +1,10 @@
 module.exports =({
 name: "slowmode",
- aliases: ["sm"],
-code: `$if[$message==;]
-$slowmode[$channelID;$messages]
+aliases: ["sm"],
+code: `$slowmode[$channelID;$messages]
 $deleteIn[5s]
 $deletecommand
 $title[Slowmode]
-$description[Set slowmode to $messages]
-$elseIf[$message!=;]
-$endElseIf
-$endIf
-$suppressErrors[Sorry but something went wrong! Please DM LaganYT#0001] $onlyIf[$hasRole[$authorID;$getVar[staffrole]]!=true;]`
+$description[Set slowmode to $messages] 
+$onlyIf[$hasRole[$authorID;$getVar[staffrole]]==true;]`
 })
