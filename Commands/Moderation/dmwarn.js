fcb293df94bb35ab20252cf315ee2a55fc478bf2
Gamerlagan-lag <70490317+Gamerlagan-lag@users.noreply.github.com>
2021-06-08 14:55:20 -0700

    Update dmwarn.js

--- Commands/Moderation/dmwarn.js
+++ Commands/Moderation/dmwarn.js
@@ -2,10 +2,10 @@ module.exports =({
       name: "warn",
  aliases: "w",
       code: `$dm[$mentioned[1]]
-      $title[You Have Been Warned In Giveaway Hub]
+      $title[You Have Been Warned In $servername]
       $description[
 You have been warned!
 Reason:$noMentionMessage
 Moderator: <@$authorID>]
-$onlyIf[$hasRole[$authorID;$getVar[staffrole]]!=true;]`
+$onlyIf[$hasRole[$authorID;$getVar[staffrole]]==true;]`
 })
