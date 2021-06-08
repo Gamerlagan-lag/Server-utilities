4d50ff738246eeb10bb53905903bae341be728ca
Gamerlagan-lag <70490317+Gamerlagan-lag@users.noreply.github.com>
2021-06-08 14:55:02 -0700

    Update warn.js

--- Commands/Moderation/warn.js
+++ Commands/Moderation/warn.js
@@ -1,17 +1,11 @@
 module.exports =({
       name: "warn",
-       ussage: "warn <user> (reason)",
- description: "warns a user for a certain reason.",
  aliases: "w",
-      code: `
-$textSplit[$message; ]
-$title[**Warn**]
-
-$description[
-User: <@$mentioned[1]> Has been warned
+      code: `$dm[$mentioned[1]]
+      $title[You Have Been Warned In $servername]
+      $description[
+You have been warned!
 Reason:$noMentionMessage
 Moderator: <@$authorID>]
-$setUserVar[warn;$sum[$getUserVar[warn;$mentioned[1]];1];$mentioned[1]]
-$suppressErrors[Sorry but something went wrong! Please DM LaganYT#0001]
 $onlyIf[$hasRole[$authorID;$getVar[staffrole]]==true;]`
 })
